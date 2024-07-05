92a5530eb1e7fb3b1ed3d7e7bfe17091d2094e02
Ranvernon <ramonoliveira2408@gmail.com>
2024-07-05 03:19:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    document.getElementById('universe').classList.add('visible');
+    document.querySelector('.container').style.display = 'none';
+});
